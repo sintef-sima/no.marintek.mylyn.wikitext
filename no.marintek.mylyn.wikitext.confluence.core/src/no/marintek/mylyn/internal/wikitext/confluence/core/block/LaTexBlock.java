6a2207bc095a36b339940ca070a689b51843d32c
Torkild U. Resheim <torkildr@gmail.com>
2013-04-23 22:32:03 +0200

    Improve LaTex handling.

--- no.marintek.mylyn.wikitext.confluence.core/src/no/marintek/mylyn/internal/wikitext/confluence/core/block/LaTexBlock.java
+++ no.marintek.mylyn.wikitext.confluence.core/src/no/marintek/mylyn/internal/wikitext/confluence/core/block/LaTexBlock.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2012 MARINTEK and others.
+ * Copyright (c) 2011, 2012, 2013 MARINTEK and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -34,6 +34,7 @@ import javax.swing.JLabel;
 
 import no.marintek.mylyn.wikitext.confluence.core.ExtendedConfluenceLanguage;
 
+import org.apache.tools.ant.BuildException;
 import org.eclipse.mylyn.internal.wikitext.confluence.core.block.AbstractConfluenceDelimitedBlock;
 import org.eclipse.mylyn.wikitext.core.parser.Attributes;
 import org.eclipse.mylyn.wikitext.core.parser.DocumentBuilder.BlockType;
@@ -49,6 +50,12 @@ import org.scilab.forge.jlatexmath.TeXIcon;
 public class LaTexBlock extends AbstractConfluenceDelimitedBlock {
 
 	private static final double INCH_2_CM = 2.54;
+
+	StringBuilder cleanedLatex = new StringBuilder();
+
+	private int dpi = 300;
+
+	private boolean imageWritten;
 
 	private String title;
 
@@ -88,7 +95,14 @@ public class LaTexBlock extends AbstractConfluenceDelimitedBlock {
 		return sb.toString();
 	}
 
-	StringBuilder cleanedLatex = new StringBuilder();
+	@Override
+	protected void endBlock() {
+		writeImage(cleanedLatex.toString());
+		if (title != null) {
+			builder.endBlock(); // panel
+		}
+		builder.endBlock(); // latex
+	}
 
 	@Override
 	protected void handleBlockContent(String content) {
@@ -96,43 +110,11 @@ public class LaTexBlock extends AbstractConfluenceDelimitedBlock {
 
 	}
 
-	private void writeImage(String content) {
-		if (imageWritten) {
-			return;
-		}
-		File root = ((ExtendedConfluenceLanguage) getMarkupLanguage()).getResourcesPath();
-		if (root == null) {
-			throw new IllegalArgumentException("No folder for LaTeX images has been specified"); //$NON-NLS-1$
-		}
-		if (!root.exists()) {
-			if (!root.mkdirs()) {
-				throw new IllegalArgumentException("Could not create folder for LaTeX images"); //$NON-NLS-1$
-			}
-		}
-		TeXFormula formula = new TeXFormula(content);
-
-		// 16pt font (?) @ 72 DPI > 300dpi
-		TeXIcon icon = formula.createTeXIcon(TeXConstants.STYLE_DISPLAY, (float) (16.0 * (dpi / 72)));
-		icon.setInsets(new Insets(5, 5, 5, 5));
-		BufferedImage image = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(), BufferedImage.TYPE_INT_ARGB);
-		Graphics2D g2 = image.createGraphics();
-		g2.setColor(Color.white);
-		g2.fillRect(0, 0, icon.getIconWidth(), icon.getIconHeight());
-		JLabel jl = new JLabel();
-		jl.setForeground(new Color(0, 0, 0));
-		icon.paintIcon(jl, g2, 0, 0);
-		String id = UUID.randomUUID().toString();
-		File file = new File(root, id + ".png"); //$NON-NLS-1$
-		ImageAttributes img = new ImageAttributes();
-		// img.setWidthPercentage(true);
-		// img.setWidth(50);
-		builder.image(img, file.getName());
-		try {
-			saveGridImage(image, file);
-			//ImageIO.write(image, "png", file.getAbsoluteFile()); //$NON-NLS-1$			
-			imageWritten = true;
-		} catch (IOException ex) {
-		}
+	@Override
+	protected void resetState() {
+		super.resetState();
+		title = null;
+		cleanedLatex.setLength(0);
 	}
 
 	private void saveGridImage(BufferedImage gridImage, File output) throws IOException {
@@ -142,7 +124,8 @@ public class LaTexBlock extends AbstractConfluenceDelimitedBlock {
 		for (Iterator<ImageWriter> iw = ImageIO.getImageWritersByFormatName(formatName); iw.hasNext();) {
 			ImageWriter writer = iw.next();
 			ImageWriteParam writeParam = writer.getDefaultWriteParam();
-			ImageTypeSpecifier typeSpecifier = ImageTypeSpecifier.createFromBufferedImageType(BufferedImage.TYPE_INT_RGB);
+			ImageTypeSpecifier typeSpecifier = ImageTypeSpecifier
+					.createFromBufferedImageType(BufferedImage.TYPE_INT_RGB);
 			IIOMetadata metadata = writer.getDefaultImageMetadata(typeSpecifier, writeParam);
 			if (metadata.isReadOnly() || !metadata.isStandardMetadataFormatSupported()) {
 				continue;
@@ -160,8 +143,6 @@ public class LaTexBlock extends AbstractConfluenceDelimitedBlock {
 			break;
 		}
 	}
-
-	private int dpi = 300;
 
 	private void setDPIMetadata(IIOMetadata metadata) throws IIOInvalidTreeException {
 
@@ -184,29 +165,45 @@ public class LaTexBlock extends AbstractConfluenceDelimitedBlock {
 		metadata.mergeTree("javax_imageio_1.0", root); //$NON-NLS-1$
 	}
 
-	private boolean imageWritten;
-
-	@Override
-	protected void endBlock() {
-		writeImage(cleanedLatex.toString());
-		if (title != null) {
-			builder.endBlock(); // panel
-		}
-		builder.endBlock(); // latex
-	}
-
-	@Override
-	protected void resetState() {
-		super.resetState();
-		title = null;
-		cleanedLatex.setLength(0);
-	}
-
 	@Override
 	protected void setOption(String key, String value) {
-		//		if (key.equals("title")) { //$NON-NLS-1$
-		// title = value;
-		// }
+	}
+
+	private void writeImage(String content) {
+		if (imageWritten) {
+			return;
+		}
+		File root = ((ExtendedConfluenceLanguage) getMarkupLanguage()).getResourcesPath();
+		if (root == null) {
+			throw new IllegalArgumentException("No folder for LaTeX images has been specified"); //$NON-NLS-1$
+		}
+		if (!root.exists()) {
+			if (!root.mkdirs()) {
+				throw new IllegalArgumentException("Could not create folder for LaTeX images"); //$NON-NLS-1$
+			}
+		}
+		try {
+			TeXFormula formula = new TeXFormula(content);
+			// 16pt font (?) @ 72 DPI > 300dpi
+			TeXIcon icon = formula.createTeXIcon(TeXConstants.STYLE_DISPLAY, (float) (16.0 * (dpi / 72)));
+			icon.setInsets(new Insets(5, 5, 5, 5));
+			BufferedImage image = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(),
+					BufferedImage.TYPE_INT_ARGB);
+			Graphics2D g2 = image.createGraphics();
+			g2.setColor(Color.white);
+			g2.fillRect(0, 0, icon.getIconWidth(), icon.getIconHeight());
+			JLabel jl = new JLabel();
+			jl.setForeground(new Color(0, 0, 0));
+			icon.paintIcon(jl, g2, 0, 0);
+			String id = UUID.randomUUID().toString();
+			File file = new File(root, id + ".png"); //$NON-NLS-1$
+			ImageAttributes img = new ImageAttributes();
+			builder.image(img, file.getName());
+			saveGridImage(image, file);
+			imageWritten = true;
+		} catch (Exception ex) {
+			throw new BuildException("Could not generate image from LaTex", ex);
+		}
 	}
 
 }
